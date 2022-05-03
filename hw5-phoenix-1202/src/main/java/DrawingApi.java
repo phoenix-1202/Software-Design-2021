5344170bc120e45f1eb2f3163b9584c64bbfeccf
Olesya Chebotareva <71000623+phoenix-1202@users.noreply.github.com>
2022-05-03 21:38:10 +0300

    update

--- hw5-phoenix-1202/src/main/java/DrawingApi.java
+++
@@ -1,9 +0,0 @@
-public interface DrawingApi {
-    long width = 1000;
-    long height = 1000;
-
-    void drawCircle(Pair<Double, Double> center, double radius);
-    void drawLine(Pair<Double, Double> from, Pair<Double, Double> to);
-
-    void plot();
-}
